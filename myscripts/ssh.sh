8c6d64d6e14eb6270624d293400c25410e504b3d
Sitao Lin <sitao@macbook.local>
2024-05-31 19:11:20 +0800

    Site updated: 2024-05-31 19:11:19

--- myscripts/ssh.sh
+++ myscripts/ssh.sh
@@ -7,8 +7,11 @@ cd ~/.ssh
 echo "$SSH_PUBLIC_KEY" > ~/.ssh/authorized_keys
 chmod 600 authorized_keys
 chmod 700 ~/.ssh
-sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+
+cat << EOF > /etc/ssh/sshd_config.d/ssh.conf
+PasswordAuthentication yes
+EOF
+
 service ssh restart
 
 echo "SSH密钥已添加并且密码登录已禁用"
