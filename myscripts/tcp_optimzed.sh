ddbb9c2ca95373f2a3eb6f1936969d81fff5c325
Sitao Lin <sitao@macbook.local>
2024-06-06 16:22:58 +0800

    Site updated: 2024-06-06 16:22:58

--- myscripts/tcp_optimzed.sh
+++ myscripts/tcp_optimzed.sh
@@ -27,6 +27,7 @@ net.ipv4.route.flush=1
 net.ipv6.conf.all.disable_ipv6 = 1
 net.ipv6.conf.default.disable_ipv6 = 1
 net.ipv6.conf.lo.disable_ipv6 = 1
+kernel.panic = -1
 #vm.swappiness = 0
 #net.core.somaxconn = 8192 #设置内核接受的连接数上限
 EOF
