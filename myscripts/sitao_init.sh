467f1e15a08bb418c518b58993d65f0a4584db27
sitao <sitaohost@gmail.com>
2024-08-27 13:10:35 +0800

    Site updated: 2024-08-27 13:10:34

--- myscripts/sitao_init.sh
+++ myscripts/sitao_init.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-echo "alias ll='ls $LS_OPTIONS -lh'" >> ~/.bashrc
 echo "export LS_OPTIONS='--color=auto'" >> ~/.bashrc
+echo "alias ls='ls $LS_OPTIONS'" >> ~/.bashrc
+echo "alias ll='ls $LS_OPTIONS -lh'" >> ~/.bashrc
 echo 'export PS1="\033[37m[\u@\h \033[33m\w\033[37m]# "' >> ~/.bashrc
 source ~/.bashrc
+echo -e "\033[36mDone.\033[0m"
