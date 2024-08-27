91065dfabf559d6c265765b2976e47370cfee2d3
sitao <sitaohost@gmail.com>
2024-08-27 13:28:39 +0800

    Site updated: 2024-08-27 13:28:39

--- myscripts/sitao_init.sh
+++ myscripts/sitao_init.sh
@@ -5,7 +5,7 @@ echo "export LS_OPTIONS='--color=auto'" >> ~/.bashrc
 echo "alias ls='ls $LS_OPTIONS'" >> ~/.bashrc
 echo "alias ll='ls $LS_OPTIONS -lh'" >> ~/.bashrc
 echo 'export PS1="\033[37m[\u@\h \033[33m\w\033[37m]# "' >> ~/.bashrc
-
+echo -e "\033[36m .bashrc配置完成.\033[0m"
 
 SSH_PUBLIC_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCxK7xG9NDFCvmSm7ktDuuDC+6sBjmRvy4dFKatGlgl9pfv0RXrQ7CDALETc22TIbPFHk+Adr1LJNWZH1X3W3QNaXXQl0XuxaZS0WuUnzZe1IDIQSLJCjHPGaUWI32pR0uNv/DN6KFkOhISnBLl+UzZGgSN+ozbORqEre1Cwbc5PT0FXx/Xuk84AySLVTSj9HhyPcoYQV/xcxwN0utjlK0rNPFavhHESm5AUzWT/1ZloyxdRejoox3sPglC7r0neQDf71Okh7ITw2oXWZrSpjRT41mgEGG5GeWBTERtTsv/VMVKzKbNdJnclqfxfehapuLUHOS0O7YFr4jVwMVudGTB2jZiKtGjBZA9u9OQF45PHoyK4ibc+ehLRjKqCzSWGfO50IQ4mctbzFInedVqSlfT3C+mqkfx1yWfh/+MGx6sMcmOXC8eIxu52XQStb69jBwe5fMgHN34W8rsmUmlNHKAty4srLZgCMsbP08+TlA2oHxnmPN8o2R3R8XicLdwnROXNuZoouoQn3VLNj8ugx3hcJsfF1f3wYoutQA4L5Qu4GsWcOZUe3TMrh4MrQZc4QOWfd2LkikAyij1yPpUXvftTrgRjKlo/OqfwDeYTYdBCai0Cr/sAp6H8+RKND2ZmUf1VWroW2xqfvWH1ljXZN+2xjEdV3SD1IdHIObBzjpuow== sitaohost@gmail.com"
 
@@ -26,8 +26,13 @@ EOF
 systemctl restart sshd
 
 echo "SSH密钥已添加并且密码登录已禁用"
+echo
+echo -e "\033[36m SSH配置完成.\033[0m"
+
 bash -c 'source ~/.bashrc; echo -e "\033[36mDone.\033[0m"'
 echo
-echo "3s后重启..."
-sleep 3
+for i in {3..1}; do
+    echo -ne "系统将在 $i 秒后重启...\r"
+    sleep 1
+done
 reboot
